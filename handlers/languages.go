e3a99b6cf3c55ea9b82e2d68a4297ab3d787e8ba
khai93 <nang.khai76gmail.com>
2022-11-08 23:57:50 -0600

    add langauges endpoint

--- handlers/languages.go
+++ handlers/languages.go
@@ -9,11 +9,26 @@ type LanguageHandler struct {
 	ExecService stella.ExecutionService
 }
 
+type LanguageView struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
 /*
 Gets the languages from the Execution Service and sends it as response
 */
 func (h LanguageHandler) GetLanguages(c *gin.Context) {
-	languages := "todo"
+	var output = []LanguageView{}
 
-	c.JSON(200, languages)
+	for _, l := range stella.Langauges {
+		var base = LanguageView{
+			Id:      l.Id,
+			Name:    l.Name,
+			Version: l.Version,
+		}
+		output = append(output, base)
+	}
+
+	c.JSON(200, output)
 }
